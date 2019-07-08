9d737500a82cd2c730b84be38bd87f3cc62b5ce3
Nathaniel Perkins <nsperkins13@gmail.com>
2019-07-07 18:47:08 -0700

    Made homepage, passwords for register working

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,7 +24,33 @@ db = scoped_session(sessionmaker(bind=engine))
 
 res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "7BlWqLEVO1nBlVDLGsl4Aw", "isbns": "9781632168146"})
 
+# class for book objects
+class book:
+	def __init__(self, title, author, pub_year, isbn, reviews, rating, num_ratings):
+		self.title = title
+		self.author = author
+		self.pub_year = pub_year
+		self.isbn = isbn
+		self.reviews = reviews
+		self.rating = rating
+		self.num_ratings = num_ratings
+
+
 # Try 
-@app.route("/")
-def index():
-    return res.json()
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+	return render_template("register.html")
+
+@app.route("/user", methods=["POST"])
+def user():
+	password = request.form.get("password")
+	confirm = request.form.get("confirm")
+	username = request.form.get("username")
+
+	if(password != confirm):
+		return render_template("register.html", nomatch=True)
+	# if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
